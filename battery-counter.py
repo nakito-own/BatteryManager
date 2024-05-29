79589fafe3a2dcc57108a2135d25f2d6c601aa73
Nakito <nikitapanov8911@gmail.com>
2024-05-29 14:06:37 +0300

    Reformat structure and mini front dev

---
+++ battery-counter.py
@@ -0,0 +1,5 @@
+from distribution import Distribution
+from route import Route
+from robot import Robot
+from address import Address
+
--- robot.py
+++ robot.py
@@ -5,17 +5,25 @@ class Robot:
     name = ''
     actual_location = Address
     garage_status = False
+    gen = ''
 
-    def __init__(self, name, actual_location, garage_status):
+    def __init__(self, name, actual_location, garage_status, gen):
         self.name = name
         self.actual_location = actual_location
         self.garage_status = garage_status
+        self.gen = gen
 
     def __str__(self):
-        return f"{self.name}, {self.actual_location}, {'в гараже' if self.garage_status else 'не в гараже'}"
+        return f"Name: {self.name}, Location: {self.actual_location}, Garage status: {'в гараже' if self.garage_status else 'не в гараже'}, Generation: {'new' if int(self.name[1:]) > 390 else 'old'}"
 
     def get_name(self):
-        return f"{self.name}"
+        return self.name
 
     def get_actual_location(self):
-        return f"{self.actual_location}"
+        return self.actual_location
+
+    def get_gen(self):
+        return self.gen
+
+    def get_info(self):
+        return self.name, self.garage_status, self.gen
